7bfe36b5b9f643c76d61171b48f6522a0745e376
Libro-goloso-07 <giovanni.carninati@gmail.com>
2023-12-02 23:31:52 +0100

    Navbar global component is done

--- src/Components/Global/Navbar/Navbar.tsx
+++ src/Components/Global/Navbar/Navbar.tsx
@@ -1,7 +1,35 @@
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import { useState } from "react";
 
-const Navbar = () => {
-  return <Link to="/">Go to Home</Link>;
+const Navbar: React.FC = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+
+  const onClickHandler = () => {
+    isMenuVisible ? setIsMenuVisible(false) : setIsMenuVisible(true);
+  };
+
+  return (
+    <nav className={styles.Navbar}>
+      <a href="">
+        <h3>MapSpawn</h3>
+      </a>
+      <ul className={`${styles.menu} ${isMenuVisible && styles.menu_visible}`}>
+        <Link to="/">HOME</Link>
+        <Link to="/">CHARACTERS</Link>
+      </ul>
+      <div
+        onClick={onClickHandler}
+        className={`${styles.hambuger_bars_div} ${
+          isMenuVisible && styles.change
+        }`}
+      >
+        <div className={styles.bar1}></div>
+        <div className={styles.bar2}></div>
+        <div className={styles.bar3}></div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
