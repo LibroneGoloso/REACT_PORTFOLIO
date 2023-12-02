eefe16d7ce8d167c7680f64c05c84eb7624632b7
Libro-goloso-07 <giovanni.carninati@gmail.com>
2023-12-02 20:22:55 +0100

    project setup done, time to start

---
+++ src/Components/Global/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return <Link to="/">Go to Home</Link>;
+};
+
+export default Navbar;
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import styles from "./Home.module.css";
+import Navbar from "../Global/Navbar/Navbar";
+
+const Home = () => {
+  return <Navbar />;
+};
+
+export default Home;
