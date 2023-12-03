53a797976d809871575fd8ccc451f0127bf6a534
Libro-goloso-07 <giovanni.carninati@gmail.com>
2023-12-03 01:49:35 +0100

    searchbar added since i forgot

--- src/Components/Global/Navbar/Navbar.tsx
+++ src/Components/Global/Navbar/Navbar.tsx
@@ -14,7 +14,19 @@ const Navbar: React.FC = () => {
       <Link to="/">
         <h3>ANTEIKU</h3>
       </Link>
+
       <ul className={`${styles.menu} ${isMenuVisible && styles.menu_visible}`}>
+        <form action="/characters" method="get">
+          <input type="text" placeholder="Search" />
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+        </form>
         <Link to="/">HOME</Link>
         <Link to="/">CHARACTERS</Link>
       </ul>
